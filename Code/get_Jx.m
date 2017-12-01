950eedaede809dab61107e5eb52052b2ec21f575
ryan_losch <ryanlosch12@gmail.com>
2017-11-30 21:17:53 -0600

    Fixes to get_Jx function from Claire

--- Code/get_Jx.m
+++ Code/get_Jx.m
@@ -6,8 +6,6 @@ if ~isstruct(FSAE_Race_Car)
 	error('The car that was input was not a FSAE_Race_Car');
 end
 
-centerOfGravity = get_cg(FSAE_Race_Car) * 12; %convert to inches
-
 massOfChassis = FSAE_Race_Car.chassis.weight / 32.174; %Divide weight by g to get mass.
 massOfMotor = FSAE_Race_Car.power_plant.weight / 32.174;
 massOfDriverLegs = (FSAE_Race_Car.pilot.weight * .4) / 32.174;
@@ -15,30 +13,30 @@ massOfDriverUpperBody = (FSAE_Race_Car.pilot.weight * .6) / 32.174;
 
 %radius and lengths of Driver
 radiusOfDriver = FSAE_Race_Car.pilot.girth / (2*pi);
-legLength = FSAE_Race_Car.pilot.height * .6;
+
 torsoLength = FSAE_Race_Car.pilot.height * .4;
 
 %Need to find the distance from the center of gravity
 chassisDistance = FSAE_Race_Car.chassis.cg_Z;
 motorDistance = FSAE_Race_Car.chassis.motor_Z;
 legDistance = FSAE_Race_Car.chassis.seat_Z + radiusOfDriver; 
-torsoDistance = FSAE_Race_Car.chassis.seat_Z + (radiusOfDriver*2) + (torsoLength / 5);
+torsoDistance = FSAE_Race_Car.chassis.seat_Z + (radiusOfDriver*2) + (FSAE_Race_Car.pilot.height/5);
 
 %calculate moment of inertias for each object.
 radiusOfChassis = FSAE_Race_Car.chassis.diameter / 2;
-lengthOfChassis = FSAE_Race_Car.chassis.length;
-inertiaOfChassis = (1/12) * massOfChassis * ((6*radiusOfChassis^2) + lengthOfChassis^2);
+
+inertiaOfChassis =  massOfChassis * ((radiusOfChassis^2));
 
 radiusOfMotor = FSAE_Race_Car.power_plant.diameter / 2;
 inertiaOfMotor = (2/5) * massOfMotor * radiusOfMotor^2;
 
-inertiaOfLegs = (1/12) * massOfDriverLegs * ((3*radiusOfDriver^2) + legLength^2);
+inertiaOfLegs = (1/2) * massOfDriverLegs * ((radiusOfDriver^2));
 inertiaOfUpperBody = (1/12) * massOfDriverUpperBody * ((3*radiusOfDriver^2) + torsoLength^2);
 
 %Calculate the (I + md^2) terms
 jFromChassis = (inertiaOfChassis + (massOfChassis * chassisDistance^2)) / 144;
 jFromMotor = (inertiaOfMotor + (massOfMotor * motorDistance^2)) / 144;
-jFromLegs = (inertiaOfLegs + (massOfDriverLegs * legDistance^2)) / 144;
+jFromLegs = (inertiaOfLegs + (massOfDriverLegs * legDistance^2)) /144;
 jFromTorso = (inertiaOfUpperBody + (massOfDriverUpperBody * torsoDistance^2)) / 144;
 
 %Sum the terms

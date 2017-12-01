6c69f82cacf35686bffc7749d7681e4ad53920ab
ryan_losch <ryanlosch12@gmail.com>
2017-12-01 00:46:55 -0600

    clean up solver

--- Code/MS2PECE.m
+++ Code/MS2PECE.m
@@ -20,36 +20,26 @@ for ii=1:dof
    A(1,ii) = A0(ii);
 end
 
-x_1p = zeros(dof,1);
-v_1p = zeros(dof,1);
-for n = 1:dof
-	x_1p(n,1) = X0(n) + h*V0(n) + (h^2/2)*A0(n);
-	v_1p(n,1) = V0(n) + h*A0(n);
-end
 [f, D] = FN(T(1), D);
+
+x_1p = X0 + h.*V0 + (h^2/2).*A0;
+v_1p = V0 + h.*A0;
 a_1p = M\(f - C*v_1p - K*x_1p);
 
 %Maybe need to refine the interval? need to ask what the tolerance is.
 %See in document that PE(CE)^m where m is supposed to be 2
 
 for m = 1:2
-	for j = 1:dof
-		x_1p(j,1) = X0(j) + (h/2)*(v_1p(j,1) + V0(j)) - (h^2/12)*(a_1p(j,1) - A0(j));
-		v_1p(j,1) = V0(j) + (h/2)*(a_1p(j,1) + A0(j));
-	end
-
+	x_1p = X0 + (h/2).*(v_1p + V0 - (h^2/12).*(a_1p - A0));
+	v_1p = V0 + (h/2).*(a_1p + A0);
 	a_1p = M\(f - C*v_1p - K*x_1p);
 end
 
 %Assign 1st predicted value into vectors?
-for jj = 1:dof
-	X(2,jj) = x_1p(jj,1);
-	V(2,jj) = v_1p(jj,1);
-	A(2,jj) = a_1p(jj,1);
-end
+X(2,:) = x_1p;
+V(2,:) = v_1p;
+A(2,:) = a_1p;
 
-x_p = zeros(dof,1);
-v_p = zeros(dof,1);
 for nn = 2:D.N-1
 	T(nn+1) = T(nn) + h;
 
